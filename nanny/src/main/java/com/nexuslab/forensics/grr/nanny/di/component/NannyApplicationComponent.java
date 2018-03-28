2096357cf867f79b76ba399bcd418d3f91c3aff7
uddhav gautam <infofuniversities@gmail.com>
2018-03-27 19:44:33 -0500

    chore(*): first add based on article (not working)

--- nanny/src/main/java/com/nexuslab/forensics/grr/nanny/di/component/NannyApplicationComponent.java
+++ nanny/src/main/java/com/nexuslab/forensics/grr/nanny/di/component/NannyApplicationComponent.java
@@ -13,21 +13,6 @@ import dagger.android.AndroidInjector;
  * Created by gaute on 3/25/18.
  */
 
-/**
- * Every type annotated with @Component must contain at least one abstract component method.
- * Component methods may have any name, but must have signatures that conform to either provision or members-injection contracts.
- * <p>
- * <p>
- * Provision methods:
- * SomeType getSomeType();
- * Provider<SomeType> getSomeTypeProvider();
- * Lazy<SomeType> getLazySomeType();
- * <p>
- * Members-injection methods
- * void injectSomeType(SomeType someType);
- * SomeType injectAndReturnSomeType(SomeType someType);
- */
-
 @Singleton
 @Component(modules = NannyApplicationModule.class)
 public interface NannyApplicationComponent extends AndroidInjector<NannyApplication> {
